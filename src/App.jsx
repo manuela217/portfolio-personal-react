39f1f51f5fc47606d4ffc85f5e23ab8b536085d9
Manuela Mendoza Barba <manuela.mendoza.dev@gmail.com>
2026-08-12 17:36:18 +0200

    Add Sport nutrition project detail page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import HandmadeShop from "./pages/HandmadeShop";
 import PortfolioReact from "./pages/PortfolioReact";
 import TaskManager from "./pages/TaskManager";
+import SportNutrition from "./pages/SportNutrition";
 import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route path="/projects/handmade-shop" element={<HandmadeShop />} />
         <Route path="/projects/portfolio-react" element={<PortfolioReact />} />
         <Route path="/projects/task-manager" element={<TaskManager />} />
+        <Route path="/projects/sport-nutrition" element={<SportNutrition />} />
       </Routes>
     </>
   );
